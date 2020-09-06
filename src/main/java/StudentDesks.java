d3b1eb37a153fb3fd3d1049f50284868e2cb772e
Joshua Temte <joshua.1854750@nv.ccsd.net>
2020-09-06 20:49:01 +0000

    Completed StudentDesks

--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -7,13 +7,17 @@ class StudentDesks {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
-        
+        int numStudentsA = scanner.nextInt();
+        int numStudentsB = scanner.nextInt();
+        int numStudentsC = scanner.nextInt();
+        int numDesks;
 
         /*
          *  your code goes here
          */
-
-
+        numDesks = (numStudentsA + numStudentsB + numStudentsC) / 2;
+        System.out.print(numDesks);
+ 
         // closing the scanner object
         scanner.close();
     }
